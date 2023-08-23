beef5f87e60c7cc675e6f8b16aa8de7691f96c0b
biancasuarz <blancasuarz@gmail.com>
2023-08-23 14:10:03 -0300

    Implementando analise de candidato através de pretensão salatial

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,9 +1,9 @@
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("Processo Seletivo");
-
+       analisarCandidato(1900.0);
+       analisarCandidato(2200.0);
+       analisarCandidato(2000.0);
     }
-
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido) {
