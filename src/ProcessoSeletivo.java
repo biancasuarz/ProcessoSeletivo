537f9ae25f043b4618271408166da583fbc6dbff
biancasuarz <blancasuarz@gmail.com>
2023-08-24 13:47:11 -0300

    Adicionando estrutura de repetição

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,8 +1,26 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
        analisarCandidato(1900.0);
        analisarCandidato(2200.0);
        analisarCandidato(2000.0);
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"Bianca","Alanis","Astrid","Zoe"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato" + candidato);
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
